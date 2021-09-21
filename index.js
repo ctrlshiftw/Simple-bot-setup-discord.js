33a84c62cda1395b6868022a420f2302a3a8dd93
ctrl shiftw <91109873+ctrlshiftw@users.noreply.github.com>
2021-09-21 06:25:00 +0000

    加入了help指令

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const express = require('express')
 const fs = require('fs')
 const keepAlive = require('./server.js')
 keepAlive();
-
+//所有準備
+const prefix = "!"
+//prefix
 client.on('ready', () => {
   console.log("I am ready")
   client.user.setUsername('Testbot001')
   client.user.setPresence({ activity: {name:"Your activity"}, status: "online"})
 });
-
-
+//機器人名字+狀態
 client.on('message', message => {
   if(message.content === "ping") {
     message.channel.send("pong")
@@ -27,5 +28,17 @@ client.on('message', message => {
     message.channel.send(embed)
     }
 });
-
+//機器人信息功能
+client.on('message', message => {
+  if(message.content === `${prefix}help`) {
+    let embed = new Discord.MessageEmbed()
+    .setTitle('Commands')
+    .setDescription('ping\nembed test')
+    .setFooter('help')
+    .setColor("RANDOM")
+    message.channel.send(embed)
+  }
+})
+//help指令
 client.login(process.env.token)
+//機器人種子
